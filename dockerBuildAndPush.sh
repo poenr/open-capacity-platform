4a81ad3ae1158bfd999939c3866b0be74839d86f
Administrator <Administrator@JSB-PIHUI>
2020-08-10 09:08:11 +0800

    修改容器私服地址

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -3,38 +3,44 @@
 mvn clean package -DskipTests
 if [ $? -ne 0 ];then exit 1; fi
 
+docker login --username=poenr@qq.com registry.cn-hangzhou.aliyuncs.com -p wEksbkhs@2019
+if [ $? -ne 0 ];then exit 1; fi
+
 cd ./register-center/eureka-server/
 mvn docker:build
-docker push  registry.apps.k8s.software.dc/ljgk/eureka-server:latest
+docker push  registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/eureka-server:latest
 cd ../../
 
 cd ./sql
-docker build -f dockerfile_auth-center -t registry.apps.k8s.software.dc/ljgk/auth-center-mysql:latest .
-docker push registry.apps.k8s.software.dc/ljgk/auth-center-mysql:latest
+docker build -f dockerfile_auth-center -t registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/auth-center-mysql:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/auth-center-mysql:latest
 
-docker build -f dockerfile_user-center -t registry.apps.k8s.software.dc/ljgk/user-center-mysql:latest .
-docker push registry.apps.k8s.software.dc/ljgk/user-center-mysql:latest
+docker build -f dockerfile_user-center -t registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/user-center-mysql:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/user-center-mysql:latest
 
-docker build -f dockerfile_log-center -t registry.apps.k8s.software.dc/ljgk/log-center-mysql:latest .
-docker push registry.apps.k8s.software.dc/ljgk/log-center-mysql:latest
+docker build -f dockerfile_log-center -t registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/log-center-mysql:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/log-center-mysql:latest
 cd ../
 
 cd ./oauth-center/auth-server
 mvn docker:build
-docker push registry.apps.k8s.software.dc/ljgk/auth-server:latest
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/auth-server:latest
 cd ../../
 
 cd ./business-center/user-center
 mvn docker:build
-docker push registry.apps.k8s.software.dc/ljgk/user-center:latest
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/user-center:latest
 cd ../../
 
 cd ./api-gateway
 mvn docker:build
-docker push registry.apps.k8s.software.dc/ljgk/api-gateway:latest
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/api-gateway:latest
 cd ../
 
 cd ./web-portal/back-center
 mvn docker:build
-docker push registry.apps.k8s.software.dc/ljgk/back-center:latest
+docker push registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/back-center:latest
 cd ../../
+
+docker pull redis:4.0.14
+docker tag redis:4.0.14 registry.cn-hangzhou.aliyuncs.com/pihui/aliyun/ljgk/redis:4.0.14
